208c7c43d1bbd2329b0da0add9f49b7db9bbbe72
Natka20 <natataradai1345@gmail.com>
2022-07-25 14:29:50 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,49 @@
+let str = ('Hello');
+console.log(str);
+alert(str);
+// document.write(str);
+str2 = ('owu');
+console.log(str2);
+alert(str2);
+str3 = ('com');
+console.log(str3);
+alert(str3);
+str4 = ('ua');
+console.log(str4);
+alert(str4);
+num1 = ('1')
+console.log(num1);
+num2 = ('10');
+console.log(num2);
+num3 = ('-999');
+console.log('123');
+num4 = ('3.14');
+console.log(num4);
+num5 = ('2.17');
+console.log(num5);
+num6 = ('16');
+console.log(num6);
+str5 = ('true');
+console.log(str5);
+str6 = ('false');
+console.log(str6);
+
+let name = ('Natalya');
+let surname = ('Chemerys');
+let lastname = ('Victorivna');
+let person = name  + surname + lastname
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let inputName = prompt('name');
+let inputMiddleName = prompt('middleName');
+let inputAge = prompt('Your Age');
+let space = ' ';
+let person2 = (inputName + space + inputMiddleName + space + inputAge);
+console.log(person2)
